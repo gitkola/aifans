21ab895bb7e6ff17c4197e1b5815b436e24fe97a
LavrovNick <lavrovmykola@gmail.com>
2024-06-11 18:44:29 +0300

    add TODO comments

--- src/api-client/userApi.ts
+++ src/api-client/userApi.ts
@@ -5,16 +5,19 @@ import axios from "axios";
 const API_URL = "/api/profile/";
 
 export const fetchUserProfile = async () => {
+  // TODO: add api handler
   const response = await axios.get(API_URL);
   return response.data;
 };
 
 export const updateUserProfile = async (profileData: any) => {
+  // TODO: add api handler, type profileData
   const response = await axios.put(API_URL, profileData);
   return response.data;
 };
 
 export const changePassword = async (passwordData: any) => {
+  // TODO: add api handler, type passwordData
   const response = await axios.put(API_URL + "change-password", passwordData);
   return response.data;
 };
